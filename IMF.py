ae1422b6a0431c647adb05f1dbec1af58298a798
Surya <pawansurya903@gmail.com>
2025-05-16 11:42:21 +0530

    Modified imf for eval data

--- IMF.py
+++ IMF.py
@@ -2,7 +2,7 @@ import os
 import soundfile as sf
 import numpy as np
 import librosa
-from Pyemd import EMD
+from pyemd import EMD
 
 # ─────── CONFIG ───────
 BASE_PATH = os.path.expanduser("~/ICSV-Surya/ICSV-Challenge/data")
